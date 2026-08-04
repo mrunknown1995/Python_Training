642dc047e453899683676b81de1af2009435a71d
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-04 22:02:47 +0300

    Added task 4

--- 01_variables/main.py
+++ 01_variables/main.py
@@ -35,3 +35,20 @@ print(
     f'Expenses = {expenses}\n'
     f'The remaining amount = {the_rem_amount}\n'
 )
+
+
+"""Task 4"""
+
+salary = int(input('Enter your salary: '))
+food = int(input('How much do you spend on food: '))
+gasoline = int(input('How much do you spend on gasoline: '))
+apartment = int(input('How much is your apartment: '))
+
+expenses = food + gasoline + apartment
+remaining_amount = salary - expenses
+
+print(
+    f'Salary = {salary}\n'
+    f'Expenses = {expenses}\n'
+    f'The remaining amount = {remaining_amount}\n'
+)
