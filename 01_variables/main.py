8c2b5f40ca5ce656138096fc2e9b2e9f2db439c1
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-04 21:52:53 +0300

    Added task 3

--- 01_variables/main.py
+++ 01_variables/main.py
@@ -12,9 +12,26 @@ print(
     f'and my weight is {weight}.'
 )
 
+
 """Task 2"""
 
 name = input('Your name: ')
 age = int(input('Your age: '))
 
 print(f'Your name is {name} and your age is {age}')
+
+
+"""Task 3"""
+
+salary = 120000
+food = 30000
+gasoline = 40000
+apartment = 20000
+expenses = food + gasoline + apartment
+the_rem_amount = salary - expenses
+
+print(
+    f'Salary = {salary}\n'
+    f'Expenses = {expenses}\n'
+    f'The remaining amount = {the_rem_amount}\n'
+)
