171c27f416985229c4fb258b5e725b2a2dec478d
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-05 20:02:58 +0300

    Added task 5 (mini calculator)

--- 02_conditions/main.py
+++ 02_conditions/main.py
@@ -57,3 +57,21 @@
 # else:
 #     print(num3)
 
+
+"""Task 5 (Mini calculator)"""
+
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+
+action = input('Select one operation from +-*/: ')
+if action == '+':
+    print(num1 + num2)
+elif action == '-':
+    print(num1 - num2)
+elif action == '*':
+    print(num1 * num2)
+elif action == '/':
+    print(num1 // num2)
+else:
+    print('The program does not support this symbol... ')
+
