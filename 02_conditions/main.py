546f99acd6624cdce30f3b99c095527d212de983
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-05 19:18:10 +0300

    Added task 3

--- 02_conditions/main.py
+++ 02_conditions/main.py
@@ -21,3 +21,13 @@
 #     print('Access granted')
 
 
+# """Task 3"""
+
+# temp = float(input('Enter temperature: '))
+
+# if temp < 0:
+#     print("It's freezing")
+# elif temp < 21:
+#     print("It's cool")
+# else:
+#     print("It's warm")
