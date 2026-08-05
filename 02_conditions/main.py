91ba28ca2ce01ab725f11c9d6fedbff8d1caa3ee
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-05 18:58:03 +0300

    Created new directory 02_conditions with main.py and 2 tasks

---
+++ 02_conditions/main.py
@@ -0,0 +1,23 @@
+# """Task 1"""
+
+# first_num = int(input('Enter first number: '))
+# second_num = int(input('Enter second number: '))
+
+# if first_num > second_num:
+#     print(f'The greater number is {first_num}')
+# elif second_num > first_num:
+#     print(f'The greater number is {second_num}')
+# elif first_num == second_num:
+#     print('Numbers are equal')
+
+
+# """Task 2"""
+
+# age = int(input('How old are you? '))
+
+# if age < 18:
+#     print('Access denied')
+# else:
+#     print('Access granted')
+
+
