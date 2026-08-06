62df1b1b67da120a26f9ede87a717244195f3d13
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-06 23:02:14 +0300

    Added new directory 03_loops with the first task

---
+++ 03_loops/main.py
@@ -0,0 +1,15 @@
+# """Task 1"""
+
+# user_input = int(input('Enter a number: '))
+# counter = 1
+# total = 0
+
+# while counter <= user_input:
+#     total += counter
+#     counter += 1
+# print(total)
+
+
+"""Task 2"""
+
+
