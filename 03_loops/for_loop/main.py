0c7c849d6cbc3df154850d53c12c3cde00907f8c
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-07 13:58:44 +0300

    Added task 6

--- 03_loops/for_loop/main.py
+++ 03_loops/for_loop/main.py
@@ -59,3 +59,15 @@
 #     multiplied_numbers.append(i * 2)
 
 # print(multiplied_numbers)
+
+
+# """Task 6"""
+
+# numbers = [3, 8, 15, 22, 7, 10, 30]
+# new_numbers = []
+
+# for i in numbers:
+#     if i > 10:
+#         new_numbers.append(i)
+
+# print(new_numbers)
