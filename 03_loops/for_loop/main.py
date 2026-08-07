ddfc74424a6ab76694d4e02d9cadffee5cb491c8
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-07 14:16:52 +0300

    Added task 7

--- 03_loops/for_loop/main.py
+++ 03_loops/for_loop/main.py
@@ -71,3 +71,15 @@
 #         new_numbers.append(i)
 
 # print(new_numbers)
+
+
+# """Task 7"""
+
+# numbers = [4, 15, 20, 7, 30, 11, 8, 50]
+# new_list = []
+
+# for i in numbers:
+#     if i % 2 == 0 or i > 10:
+#         new_list.append(i)
+
+# print(new_list)
