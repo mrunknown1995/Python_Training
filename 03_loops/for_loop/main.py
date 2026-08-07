9236edc15e562ee29a64c59fc99111227c9f351a
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-07 13:26:29 +0300

    Added task 3

--- 03_loops/for_loop/main.py
+++ 03_loops/for_loop/main.py
@@ -13,3 +13,23 @@
 # for name in names:
 #     print(f'Hello, {name.upper()}!')
 
+
+# """Task 3"""
+
+# numbers = [3, 8, 15, 22, 7, 10]
+
+# for num in numbers:
+#     if num > 10:
+#         print(num)
+
+# numbers = range(1, 11)
+
+# for i in numbers:
+#     print(numbers[-i])
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+# for i in range(3):
+#     for x in range(3):
+#         print(i, x)
