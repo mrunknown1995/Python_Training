c38d3bca0187e8610c50bea9c5b6362c9305cf8e
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-07 13:44:13 +0300

    Added task 5

--- 03_loops/for_loop/main.py
+++ 03_loops/for_loop/main.py
@@ -48,3 +48,14 @@
 #     total += i
 
 # print(total)
+
+
+# """Task 5"""
+
+# numbers = [1, 2, 3, 4, 5]
+# multiplied_numbers = []
+
+# for i in numbers:
+#     multiplied_numbers.append(i * 2)
+
+# print(multiplied_numbers)
