f3c778c9f08c7eb13470691ea05e8552301176e7
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-07 09:30:55 +0300

    Added task 2

--- 03_loops/main.py
+++ 03_loops/main.py
@@ -10,6 +10,15 @@
 # print(total)
 
 
-"""Task 2"""
+# """Task 2"""
 
+# total = 0
 
+# while True:
+#     user_number = int(input('Type numbers one by one: '))
+#     if user_number == 0:
+#         break
+
+#     total += user_number
+
+# print(total)
