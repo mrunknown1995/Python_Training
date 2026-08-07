e0dd02b1d7ec077a970d71386fe7b5b4c0e00b8b
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-07 10:25:11 +0300

    Added task 5

--- 03_loops/main.py
+++ 03_loops/main.py
@@ -53,3 +53,16 @@
 #     total += user_number
 
 # print(total)
+
+
+# """Task 5"""
+
+# while True:
+#     user_number = int(input('Enter a number: '))
+
+#     if user_number == 0:
+#         break
+#     elif user_number % 2 == 0:
+#         print('even')
+#     else:
+#         print('odd')
