23346623cec6d0e445d8973d474b4b23ca89bb21
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-07 12:41:25 +0300

    Added task 2

--- 03_loops/for_loop/main.py
+++ 03_loops/for_loop/main.py
@@ -5,3 +5,11 @@
 # for car in cars:
 #     print(f'I like {car}')
 
+
+# """Task 2"""
+
+# names = ["John", "Alex", "Maria", "Anna"]
+
+# for name in names:
+#     print(f'Hello, {name.upper()}!')
+
