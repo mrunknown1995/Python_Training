8f27da3f2af44fefea3fac04f59825b357016edb
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-08 19:51:39 +0300

    Added task 3

--- 03_loops/nested_loops/main.py
+++ 03_loops/nested_loops/main.py
@@ -1,4 +1,3 @@
-
 # """Task 1"""
 
 # for row in range(1, 6):
@@ -7,10 +6,25 @@
 #     print()
 
 
-"""Task 2"""
+# """Task 2"""
 
-for row in range(5, 0, -1):
-    for symbol in range(row):
-        print("*", end="")
-    print()
+# for row in range(5, 0, -1):
+#     for symbol in range(row):
+#         print('*', end='')
+#     print()
 
+
+# """Task 3"""
+
+# students = [
+#     ["John", "Python", "Git"],
+#     ["Alex", "Linux", "Docker"],
+#     ["Maria", "HTML", "CSS"]
+# ]
+
+# for inner_list in students:
+#     print(f'{inner_list[0]}:')
+
+#     for i in range(1, len(inner_list)):
+#         print(inner_list[i])
+#     print()
