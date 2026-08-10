48d6a71f93f6ddd439238d0d42f62280ec7aaf71
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-10 20:40:01 +0300

    Added task 8

--- 04_functions/main.py
+++ 04_functions/main.py
@@ -104,3 +104,24 @@
 # print(filter_numbers([8, 17, 200, 57, 0, 41, 2, 50]))
 
 
+# """Task 8"""
+
+# def second_largest_num(numbers):
+
+#     largest = 0
+#     second_largest = 0
+
+#     for i in numbers:
+
+#         if i > largest:
+#             second_largest = largest
+#             largest = i
+#         elif i > second_largest:
+#             second_largest = i
+
+#     return largest, second_largest
+
+# print(second_largest_num([10, 5, 8, 20, 15]))
+# print(second_largest_num([3, 7, 2, 9, 4, 10]))
+# print(second_largest_num([101, 50, 8, 20, 15]))
+# print(second_largest_num([[-10, -5, -20]]))
