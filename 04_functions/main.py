34f7e43e00f55faf05ef1b5cc977a023ebbfdf3c
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-11 20:18:18 +0300

    Added task 11

--- 04_functions/main.py
+++ 04_functions/main.py
@@ -165,3 +165,20 @@
 # print(find_longest_word(["cat", "elephant", "dog", "tiger"]))
 # print(find_longest_word(["Python", "Java", "JavaScript"]))
 
+
+# """Task 11"""
+
+# def filter_long_words(words, min_length):
+
+#     new_list = []
+
+#     for word in words:
+#         if len(word) >= min_length:
+#             new_list.append(word)
+
+#     return new_list
+
+
+# print(filter_long_words(["cat", "elephant", "dog", "tiger"], 5))
+# print(filter_long_words(["Python", "Go", "Java", "JavaScript"], 5))
+
