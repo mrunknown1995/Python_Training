1faff570b84e79dc87981563f6268155cb47fd11
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-11 19:55:41 +0300

    Added task 10

--- 04_functions/main.py
+++ 04_functions/main.py
@@ -145,3 +145,23 @@
 # print(count_vowels('programming'))
 # print(count_vowels('Python'))
 # print(count_vowels('JOHN'))
+
+
+# """Task 10"""
+
+# def find_longest_word(words):
+
+#     max_length = 0
+#     longest_word = ''
+
+#     for word in words:
+
+#         if len(word) > max_length:
+#             longest_word = word
+#             max_length = len(word)
+
+#     return longest_word
+
+# print(find_longest_word(["cat", "elephant", "dog", "tiger"]))
+# print(find_longest_word(["Python", "Java", "JavaScript"]))
+
