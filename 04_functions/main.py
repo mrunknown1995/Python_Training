358d49e56ac61d6c5af50b1e21336fe337d77411
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-11 22:20:47 +0300

    Added task 14

--- 04_functions/main.py
+++ 04_functions/main.py
@@ -214,3 +214,26 @@
 # print(find_first_match([10, 20, 30, 20], 20))
 # print(find_first_match(["Python", "Java", "C++"], "C++"))
 # print(find_first_match(["Python", "Java", "C++"], "rat"))
+
+
+# """Task 14"""
+
+# def get_unique_items(items):
+
+#     new_list = []
+
+#     for i in items:
+#         if i not in new_list:
+#             new_list.append(i)
+
+#     return new_list
+
+
+# print(get_unique_items([1, 2, 2, 3, 1, 4, 3]))
+# # [1, 2, 3, 4]
+
+# print(get_unique_items(["apple", "banana", "apple", "orange", "banana"]))
+# # ["apple", "banana", "orange"]
+
+# print(get_unique_items(["Python", "Python", "Java", "Python"]))
+# # ["Python", "Java"]
