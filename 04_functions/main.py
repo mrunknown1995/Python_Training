8cb4d8727adb443220ac5e82dd003d6d145f4862
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-11 20:39:04 +0300

    Added task 12

--- 04_functions/main.py
+++ 04_functions/main.py
@@ -182,3 +182,20 @@
 # print(filter_long_words(["cat", "elephant", "dog", "tiger"], 5))
 # print(filter_long_words(["Python", "Go", "Java", "JavaScript"], 5))
 
+
+# """Task 12"""
+
+# def count_occurrences(items, target):
+
+#     counter = 0
+
+#     for item in items:
+#         if item == target:
+#             counter += 1
+
+#     return counter
+
+
+# print(count_occurrences([1, 2, 3, 2, 4, 2], 2))
+# print(count_occurrences(["apple", "banana", "apple", "orange"], "apple"))
+# print(count_occurrences(["Python", "Java", "Python", "Python"], "Python"))
