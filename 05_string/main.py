0456df76021a0a7785cea398555afba179227f73
John <88924405+mrunknown1995@users.noreply.github.com>
2026-08-12 18:58:17 +0300

    Added task 1

--- 05_string/main.py
+++ 05_string/main.py
@@ -1,3 +1,10 @@
 
-"""Task 1"""
+# """Task 1"""
 
+# def analyze_text(text):
+
+#     return text.lower(), len(text), text[0]
+
+# print(analyze_text("Python"))
+# print(analyze_text("PYTHON"))
+# print(analyze_text("ChatGPT"))
