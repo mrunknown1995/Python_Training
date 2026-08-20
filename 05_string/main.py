e17b09c7d04ac7bf536bbd56d92fb30a4c0885ef
GJG <grigoryanjon1995@gmail.com>
2026-08-20 19:13:55 +0300

    Added task 9

--- 05_string/main.py
+++ 05_string/main.py
@@ -124,3 +124,15 @@
 
 # print(get_words("I love programming"))
 # # ["I", "love", "programming"]
+
+
+# """Task 9"""
+
+# def join_words(words):
+#     return " ".join(words)
+
+# print(join_words(["Python", "is", "awesome"]))
+# # "Python is awesome"
+
+# print(join_words(["I", "love", "programming"]))
+# # "I love programming"
