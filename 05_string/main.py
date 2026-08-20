8291d65d2e7c0c85707c3ddf88cb63392393cfa5
GJG <grigoryanjon1995@gmail.com>
2026-08-20 19:05:39 +0300

    Added task 7, 7(improved)

--- 05_string/main.py
+++ 05_string/main.py
@@ -77,3 +77,38 @@
 
 # print(replace_word("banana", "a", "o"))
 # # "bonono"
+
+
+"""Task 7"""
+
+def contains_word(text, word):
+
+    if word in text:
+        return True
+    else:
+        return False
+
+print(contains_word("I love Python", "Python"))
+# True
+
+print(contains_word("I love Python", "Java"))
+# False
+
+print(contains_word("Hello world", "world"))
+# True
+
+
+"""Task 7 (improved)"""
+
+def contains_word(text, word):
+
+    return word in text
+
+print(contains_word("I love Python", "Python"))
+# True
+
+print(contains_word("I love Python", "Java"))
+# False
+
+print(contains_word("Hello world", "world"))
+# True
