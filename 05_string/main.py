f15b1e708d48b7b6f214cc7edb7b5b0eadc9bd1b
GJG <grigoryanjon1995@gmail.com>
2026-08-20 18:19:03 +0300

    Added task 3

--- 05_string/main.py
+++ 05_string/main.py
@@ -10,13 +10,28 @@
 # print(analyze_text("ChatGPT"))
 
 
-"""Task 2"""
+# """Task 2"""
 
-def get_last_three(text):
-    return text[-3:]
+# def get_last_three(text):
+#     return text[-3:]
         
-print(get_last_three("Python"))
-# "hon"
+# print(get_last_three("Python"))
+# # "hon"
 
-print(get_last_three("ChatGPT"))
-# "GPT"
+# print(get_last_three("ChatGPT"))
+# # "GPT"
+
+
+# """Task 3"""
+
+# def first_and_last(text):
+#     return text[0], text[-1]
+
+# print(first_and_last("Python"))
+# # ("P", "n")
+
+# print(first_and_last("ChatGPT"))
+# # ("C", "T")
+
+# print(first_and_last("hello"))
+# # ("h", "o")
