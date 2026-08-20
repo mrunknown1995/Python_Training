03ec06f3125058713b3112d2dc8b834e20fe0190
GJG <grigoryanjon1995@gmail.com>
2026-08-20 19:08:55 +0300

    Added task 8

--- 05_string/main.py
+++ 05_string/main.py
@@ -79,36 +79,48 @@
 # # "bonono"
 
 
-"""Task 7"""
+# """Task 7"""
 
-def contains_word(text, word):
+# def contains_word(text, word):
 
-    if word in text:
-        return True
-    else:
-        return False
+#     if word in text:
+#         return True
+#     else:
+#         return False
 
-print(contains_word("I love Python", "Python"))
-# True
+# print(contains_word("I love Python", "Python"))
+# # True
 
-print(contains_word("I love Python", "Java"))
-# False
+# print(contains_word("I love Python", "Java"))
+# # False
 
-print(contains_word("Hello world", "world"))
-# True
+# print(contains_word("Hello world", "world"))
+# # True
 
 
-"""Task 7 (improved)"""
+# """Task 7 (improved)"""
 
-def contains_word(text, word):
+# def contains_word(text, word):
 
-    return word in text
+#     return word in text
 
-print(contains_word("I love Python", "Python"))
-# True
+# print(contains_word("I love Python", "Python"))
+# # True
 
-print(contains_word("I love Python", "Java"))
-# False
+# print(contains_word("I love Python", "Java"))
+# # False
 
-print(contains_word("Hello world", "world"))
-# True
+# print(contains_word("Hello world", "world"))
+# # True
+
+
+# """Task 8"""
+
+# def get_words(text):
+#     return text.split()
+
+# print(get_words("Python is awesome"))
+# # ["Python", "is", "awesome"]
+
+# print(get_words("I love programming"))
+# # ["I", "love", "programming"]
