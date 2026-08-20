fbeaacd307f388482f312ace8e22e7a6cf15a080
GJG <grigoryanjon1995@gmail.com>
2026-08-20 19:49:22 +0300

    Added task 1

---
+++ 06_lists/main.py
@@ -0,0 +1,13 @@
+
+# """Task 1"""
+
+# def add_item(items, item):
+#     items.append(item)
+#     return items
+
+# print(add_item(["Python", "Java"], "C++"))
+# # ["Python", "Java", "C++"]
+
+# print(add_item([1, 2, 3], 4))
+# # [1, 2, 3, 4]
+
