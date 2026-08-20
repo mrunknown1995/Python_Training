773d767585e265d30df3633cfab3717fcd0706ad
GJG <grigoryanjon1995@gmail.com>
2026-08-20 20:34:09 +0300

    Added task 3

--- 06_lists/main.py
+++ 06_lists/main.py
@@ -24,3 +24,21 @@
 
 # print(remove_item([1, 2, 3, 2], 2))
 # # [1, 3, 2]
+
+
+# """Task 3"""
+
+# def update_item(items, index, new_value):
+
+#     items[index] = new_value
+#     return items 
+    
+
+# print(update_item(["Python", "Java", "C++"], 1, "Go"))
+# # ["Python", "Go", "C++"]
+
+# print(update_item([10, 20, 30], 0, 100))
+# # [100, 20, 30]
+
+# print(update_item(["cat", "dog", "bird"], -1, "fish"))
+# # ["cat", "dog", "fish"]
