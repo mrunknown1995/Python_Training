964b3889a4c9bcf3053872d44a832d48eb59c5d7
GJG <grigoryanjon1995@gmail.com>
2026-08-20 19:58:29 +0300

    Added task 2

--- 06_lists/main.py
+++ 06_lists/main.py
@@ -11,3 +11,16 @@
 # print(add_item([1, 2, 3], 4))
 # # [1, 2, 3, 4]
 
+
+# """Task 2"""
+
+# def remove_item(items, item):
+
+#     items.remove(item)
+#     return items
+
+# print(remove_item(["Python", "Java", "C++", "Java"], "Java"))
+# # ["Python", "C++", "Java"]
+
+# print(remove_item([1, 2, 3, 2], 2))
+# # [1, 3, 2]
