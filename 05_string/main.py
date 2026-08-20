7a3fea932aa9c46faf724268c47b2067c7496d36
GJG <grigoryanjon1995@gmail.com>
2026-08-20 18:56:36 +0300

    Added task 6

--- 05_string/main.py
+++ 05_string/main.py
@@ -62,3 +62,18 @@
 
 # print(clean_text("   ChatGPT"))
 # # "chatgpt"
+
+
+# """Task 6"""
+
+# def replace_word(text, old, new):
+#     return text.replace(old, new)
+
+# print(replace_word("I like Python", "Python", "Linux"))
+# # "I like Linux"
+
+# print(replace_word("I love cats. Cats are great.", "Cats", "Dogs"))
+# # "I love cats. Dogs are great."
+
+# print(replace_word("banana", "a", "o"))
+# # "bonono"
