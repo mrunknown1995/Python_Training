4fe67854edcaeab0c037aa5b0f97e36df232c68e
GJG <grigoryanjon1995@gmail.com>
2026-08-20 18:49:56 +0300

    Added task 5

--- 05_string/main.py
+++ 05_string/main.py
@@ -47,3 +47,18 @@
 
 # print(reverse_text("ChatGPT"))
 # # "TPGtahC"
+
+
+# """Task 5"""
+
+# def clean_text(text):
+#     return text.lstrip().rstrip().lower()
+    
+# print(clean_text("  Python  "))
+# # "python"
+
+# print(clean_text("  HELLO WORLD  "))
+# # "hello world"
+
+# print(clean_text("   ChatGPT"))
+# # "chatgpt"
