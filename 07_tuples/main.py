3fd9ede36e9ab53779b9ef90abc5e6a0c0277e13
GJG <grigoryanjon1995@gmail.com>
2026-08-22 20:10:35 +0300

    Added task 2

--- 07_tuples/main.py
+++ 07_tuples/main.py
@@ -11,3 +11,21 @@
 # print(f"Name: {user_name}")
 # print(f"Age: {user_age}")
 # print(f"Language: {user_prog_lang}")
+
+
+# """Task 2"""
+
+# book_title = input("Enter book title: ")
+# book_author = input("Enter author: ")
+# book_pub_year = input("Enter publication year: ")
+
+# book_tuple = book_title, book_author, book_pub_year
+
+# def book_info(readable_tuple):
+
+#     print("\n--- Book Information ---")
+#     print(f"Title: {readable_tuple[0]}")
+#     print(f"Author: {readable_tuple[1]}")
+#     print(f"Year: {readable_tuple[2]}")
+
+# book_info(book_tuple)
