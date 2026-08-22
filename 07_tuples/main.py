c38492bd7587557e6bf73bdb58abff7d4f379889
GJG <grigoryanjon1995@gmail.com>
2026-08-22 19:49:02 +0300

    Created new folder > main.py > task 1

---
+++ 07_tuples/main.py
@@ -0,0 +1,13 @@
+
+# """Task 1"""
+
+# user_name = input("Enter your name: ")
+# user_age = input("Enter your age: ")
+# user_prog_lang = input("Enter your programming language: ")
+
+# user_info = (user_name, user_age, user_prog_lang)
+
+# print(f"User: {user_info}")
+# print(f"Name: {user_name}")
+# print(f"Age: {user_age}")
+# print(f"Language: {user_prog_lang}")
