8211eb3a79ab04bc5b8573196912cfa2c424b066
GJG <grigoryanjon1995@gmail.com>
2026-08-23 20:46:20 +0300

    Add task 1

---
+++ 08_sets/main.py
@@ -0,0 +1,16 @@
+# """"Task 1"""
+
+# prog_lang_set = set()
+
+# for i in range(5):
+
+#     user_input = input("Enter a programming language: ")
+#     prog_lang_set.add(user_input)
+
+# print(f"Languages: {prog_lang_set}")
+# print(f"Number of unique languages: {len(prog_lang_set)}")
+
+
+
+
+
