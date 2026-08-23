999cb0c5d8f81ab8cf9955c036b777ba3ebb4713
GJG <grigoryanjon1995@gmail.com>
2026-08-23 21:08:25 +0300

    Add task 2

--- 08_sets/main.py
+++ 08_sets/main.py
@@ -11,6 +11,25 @@
 # print(f"Number of unique languages: {len(prog_lang_set)}")
 
 
+# """Task 2"""
 
+# user1_set = set()
+
+# for i in range(3):
+#     user1_input = input("Enter a programming language: ")
+#     user1_set.add(user1_input)
+
+# user2_set = set()
+
+# for i in range(3):
+#     user2_input = input("Enter a programming language: ")
+#     user2_set.add(user2_input)
+
+# common_langs = user1_set.intersection(user2_set)
+
+# if common_langs:
+#     print(f"Common languages: {common_langs}")
+# else:
+#     print("You have no programming languages in common.")
 
 
