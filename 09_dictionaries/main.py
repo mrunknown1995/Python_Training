b96865b34913867f6c79efe913c6106c25c831fe
GJG <grigoryanjon1995@gmail.com>
2026-08-24 19:02:13 +0300

    Add task 1

---
+++ 09_dictionaries/main.py
@@ -0,0 +1,19 @@
+
+# """Task 1"""
+
+# user_name = input("Enter your name: ")
+# user_age = input("Enter your age: ")
+# user_prog_lang = input("Enter your programming language: ")
+
+# user_info = {
+#     "name": user_name,
+#     "age": user_age,
+#     "language": user_prog_lang
+# }
+
+# print()
+# print("--- User Profile ---")
+# print(f"Name: {user_info["name"]}")
+# print(f"Age: {user_info["age"]}")
+# print(f"Language: {user_info["language"]}")
+
