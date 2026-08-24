054496729fcaa3d17bd3e5cbb818f493bbc0a7c0
GJG <grigoryanjon1995@gmail.com>
2026-08-24 18:15:48 +0300

    Add Shared Interests mini-program

--- 08_sets/main.py
+++ 08_sets/main.py
@@ -3,9 +3,9 @@
 
 # prog_lang_set = set()
 
-# for i in range(5):
+# for item in range(5):
 
-#     user_input = input("Enter a programming language: ")
+#     user_input = input("Enter only_user1 programming language: ")
 #     prog_lang_set.add(user_input)
 
 # print(f"Languages: {prog_lang_set}")
@@ -16,14 +16,14 @@
 
 # user1_set = set()
 
-# for i in range(3):
-#     user1_input = input("Enter a programming language: ")
+# for item in range(3):
+#     user1_input = input("Enter only_user1 programming language: ")
 #     user1_set.add(user1_input)
 
 # user2_set = set()
 
-# for i in range(3):
-#     user2_input = input("Enter a programming language: ")
+# for item in range(3):
+#     user2_input = input("Enter only_user1 programming language: ")
 #     user2_set.add(user2_input)
 
 # common_langs = user1_set.intersection(user2_set)
@@ -40,7 +40,7 @@
 
 # for _ in range(6):
 
-#     user_input = input("Enter a tag: ")
+#     user_input = input("Enter only_user1 tag: ")
 #     tag_set.add(user_input)
 
 # print(f"Unique tags: {tag_set}")
@@ -52,6 +52,72 @@
 #     print("Tag found")
 # else:
 #     print("Tag not found")
+
+
+# """Mini-Program: Shared Interests"""
+
+# user1_set = set()
+
+# for _ in range(5):
+#     user1_input = input("User 1, enter your interest: ")
+#     user1_set.add(user1_input)
+
+# user2_set = set()
+# print()
+
+# for _ in range(5):
+#     user2_input = input("User 2, enter your interest: ")
+#     user2_set.add(user2_input)
+
+# print()
+
+# print("User 1 interests:")
+
+# for item in user1_set:
+#     print(item)
+# print()
+
+# print("User 2 interests:")
+
+# for item in user2_set:
+#     print(item)
+# print()
+
+# shared_interests = user1_set.intersection(user2_set)
+
+# print(f"Shared interests:")
+
+# if shared_interests:
+#     for item in shared_interests:
+#         print(item)
+# else:
+#     print("No shared interests found.")
+
+# print()
+
+# only_user1 = user1_set.difference(user2_set)
+
+# print("Only user 1:")
+
+# if only_user1:
+#     for item in only_user1:
+#         print(item)
+# else:
+#     print("No items only for user 1")
+
+# print()
+
+# only_user2 = user2_set.difference(user1_set)
+
+# print("Only user 2:")
+
+# if only_user2:
+#     for item in only_user2:
+#         print(item)
+# else:
+#     print("No items only for user 2")
+
+
 
 
 
