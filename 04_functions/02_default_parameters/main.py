307c0b11042a9d476f55aec887478fef19fb87be
GJG <grigoryanjon1995@gmail.com>
2026-08-26 20:07:19 +0300

    Add task 1

---
+++ 04_functions/02_default_parameters/main.py
@@ -0,0 +1,13 @@
+
+"""Task 1"""
+
+def create_profile(name, language="Python", level="Beginner"):
+
+    print()
+    print(f"Name: {name}")
+    print(f"Language: {language}")
+    print(f"Level: {level}")
+
+create_profile("Alex")
+create_profile("Alex", "Go")
+create_profile("Alex", "C++", "Advanced")
