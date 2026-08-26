79450e84b6550e90b538515582eac39869e897fb
GJG <grigoryanjon1995@gmail.com>
2026-08-26 20:51:35 +0300

    Add task 2

--- 04_functions/02_default_parameters/main.py
+++ 04_functions/02_default_parameters/main.py
@@ -1,13 +1,25 @@
 
-"""Task 1"""
+# """Task 1"""
 
-def create_profile(name, language="Python", level="Beginner"):
+# def create_profile(name, language="Python", level="Beginner"):
 
-    print()
-    print(f"Name: {name}")
-    print(f"Language: {language}")
-    print(f"Level: {level}")
+#     print()
+#     print(f"Name: {name}")
+#     print(f"Language: {language}")
+#     print(f"Level: {level}")
 
-create_profile("Alex")
-create_profile("Alex", "Go")
-create_profile("Alex", "C++", "Advanced")
+# create_profile("Alex")
+# create_profile("Alex", "Go")
+# create_profile("Alex", "C++", "Advanced")
+
+
+# """Task 2"""
+
+# def calculate_price(price, quantity=1, discount=0):
+
+#     result = price * quantity - (price * quantity * discount / 100)
+#     return result
+
+# print(calculate_price(100))
+# print(calculate_price(100, 3))
+# print(calculate_price(100, 3, 20))
