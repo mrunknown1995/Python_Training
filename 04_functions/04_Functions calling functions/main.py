9e1994344aa47d00ff7431c6308d5346de46fc02
GJG <grigoryanjon1995@gmail.com>
2026-09-01 18:17:10 +0300

    Add task 1

---
+++ 04_functions/04_Functions calling functions/main.py
@@ -0,0 +1,15 @@
+
+# """Task 1"""
+
+# def calculate_total(price, quantity):
+#     return price * quantity
+
+# def print_order(price, quantity):
+
+#     total = calculate_total(price, quantity)
+
+#     print(f"Price: {price}")
+#     print(f"Quantity: {quantity}")
+#     print(f"Total: {total}")
+
+# print_order(100, 3)
