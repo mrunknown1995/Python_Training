c60da11b829e5fa4f04df20a8780fc1aa19d2a2b
GJG <grigoryanjon1995@gmail.com>
2026-09-02 20:54:59 +0300

    Add task 2

--- 04_functions/04_Functions calling functions/main.py
+++ 04_functions/04_Functions calling functions/main.py
@@ -13,3 +13,35 @@
 #     print(f"Total: {total}")
 
 # print_order(100, 3)
+
+
+# """Task 2"""
+
+# def order_price_calc(product_price, quantity):
+#     return product_price * quantity
+    
+# def input_price_and_quantity():
+    
+#     user_input_price = int(input("Enter price: "))
+#     user_input_quantity = int(input("Enter quantity: "))
+
+#     total_price = order_price_calc(user_input_price, user_input_quantity)
+#     return total_price
+
+# def show_final_result():
+
+#     total_price = input_price_and_quantity()
+
+#     if total_price >= 1000:
+#         final_price = total_price - total_price * 10 / 100
+#     else:
+#         final_price = total_price
+
+#     print(f"total: {total_price}")
+#     print(f"Final price: {final_price}")
+
+# show_final_result()
+
+
+
+
