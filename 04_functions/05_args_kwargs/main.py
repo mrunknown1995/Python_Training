350fe24a5cd9bc4f5c6226a82ec5e70933ec0c16
GJG <grigoryanjon1995@gmail.com>
2026-09-04 19:04:37 +0300

    Add task 1

---
+++ 04_functions/05_args_kwargs/main.py
@@ -0,0 +1,18 @@
+
+# """Task 1"""
+
+# def calculate_sum(*args):
+#     res = 0
+
+#     for i in args:
+#         res += i
+#     return res
+
+# print(calculate_sum(10, 20))
+# # 30
+
+# print(calculate_sum(1, 2, 3, 4, 5))
+# # 15
+
+# print(calculate_sum(100))
+# # 100
