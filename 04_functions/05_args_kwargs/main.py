7bdf330f6dd21806cff0479916a562e065289b05
GJG <grigoryanjon1995@gmail.com>
2026-09-04 19:48:42 +0300

    Add task 2

--- 04_functions/05_args_kwargs/main.py
+++ 04_functions/05_args_kwargs/main.py
@@ -16,3 +16,30 @@
 
 # print(calculate_sum(100))
 # # 100
+
+
+# """Task 2"""
+
+# def pos_or_neg_nums(*args):
+
+#     pos_nums = 0
+#     neg_nums = 0
+
+#     for i in args:
+#         if i > 0:
+#             pos_nums += 1
+#         elif i < 0:
+#             neg_nums += 1
+
+#     return len(args), pos_nums, neg_nums
+
+# print(pos_or_neg_nums(10, -5, 0, 7, -2))
+# # (5, 2, 2)
+
+# print(pos_or_neg_nums(1, 2, 3))
+# # (3, 3, 0)
+
+# print(pos_or_neg_nums(-10, -20))
+# # (2, 0, 2)
+
+# print(pos_or_neg_nums(10, 10, -5, -5))
